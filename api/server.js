8312cffed2f3ec12fc9d07fbfefce3365c5bc0db
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-24 00:26:00 -0800

    server registers and logs in

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+const authRouter = require('../auth/auth-router.js');
+
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Backend 🤘🏼💀✨'})
